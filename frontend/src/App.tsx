c435e9514a9115df7468e8d98a50679a517651f5
Seth Jackson <sethalicious@gmail.com>
2023-12-29 10:24:25 -0800

    individual bill page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,22 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import BillsPage from "./pages/BillsPage";
 import Header from "./components/Header";
+import { useState } from "react";
+import TESTBILLS from "./assets/testData/TESTBILLS";
+import BillPage from "./pages/BillPage";
 
 function App() {
+  const [bills, setBills] = useState(TESTBILLS);
   return (
     <>
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="bills" element={<BillsPage />} />
+        <Route
+          path="bills"
+          element={<BillsPage bills={bills} setBills={setBills} />}
+        />
+        <Route path="/bills/:billId" element={<BillPage bills={bills} />} />
       </Routes>
     </>
   );
